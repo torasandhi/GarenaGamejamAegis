2dc5af2c5a9655432f06a90d184d0a0020ac3c11
torasandhi <torasandhipakey@gmail.com>
2026-02-06 19:52:34 +0700

    Init

---
+++ Source/AegisGarenaGamejam/AegisGarenaGamejam.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "AegisGarenaGamejam.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, AegisGarenaGamejam, "AegisGarenaGamejam" );
